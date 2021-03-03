d636e9ad88e71f9c1372f031ee34cc9991b130ee
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-03 16:13:52 -0700

    planet has moons

--- server/controllers/PlanetController.js
+++ server/controllers/PlanetController.js
@@ -1,19 +1,28 @@
 import express from "express";
 import BaseController from "../utils/BaseController";
 import { planetService } from "../services/PlanetService";
-//import moon
+import { moonService } from "../services/MoonService";
 
 export class planetController extends BaseController{
      constructor(){
          super("api/planet")
          this.router
             .get("", this.getAll)
+            .get("/:id/moon", this.getAllMoonsByPlanetId)
             .post("", this.create)
      }
 
      async getAll(req, res, next){
          try {
              return res.send(await planetService.find(req.query))
+         } catch (error) {
+             next(error)
+         }
+     }
+
+     async getAllMoonsByPlanetId(req, res, next){
+         try {
+             res.send(await moonService.find({ planet: req.params.id }))
          } catch (error) {
              next(error)
          }
