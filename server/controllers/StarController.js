76992e7a8d3f5864d4742847684cde3c4270c3dd
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-03 16:11:00 -0700

    stars have planets

--- server/controllers/StarController.js
+++ server/controllers/StarController.js
@@ -1,19 +1,28 @@
 import express from "express";
 import BaseController from "../utils/BaseController";
 import { starService } from "../services/StarService";
-//import planet
+import { planetService } from "../services/PlanetService";
 
 export class starController extends BaseController{
      constructor(){
          super("api/star")
          this.router
             .get("", this.getAll)
+            .get("/:id/planet", this.getAllPlanetsByStarId)
             .post("", this.create)
      }
 
      async getAll(req, res, next){
          try {
              return res.send(await starService.find(req.query))
+         } catch (error) {
+             next(error)
+         }
+     }
+
+     async getAllPlanetsByStarId(req, res, next){
+         try {
+            res.send(await planetService.find({ star: req.params.id }));
          } catch (error) {
              next(error)
          }
