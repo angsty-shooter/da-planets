165d13f6d4eeb3ff973d2be22531611bba31939d
Ashley Herrera <ashleyherrera131@gmail.com>
2021-03-03 14:38:10 -0700

    controller

---
+++ server/controllers/GalaxyController.js
@@ -0,0 +1,28 @@
+import express from "express";
+import BaseController from "../utils/BaseController";
+import { galaxyService } from "../services/GalaxyService";
+
+export class galaxyController extends BaseController{
+     constructor(){
+         super("api/galaxy")
+         this router
+            .get("", this.getAll)
+            .post("", this.create)
+     }
+
+     async getAll(req, res, next){
+         try {
+             return res.send(await galaxyService.find(req.query))
+         } catch (error) {
+             next(error)
+         }
+     }
+
+     async create(req, res, next){
+         try {
+             res.send(201, await galaxyService.create(req.query))
+         } catch (error) {
+             next(error)
+         }
+     }
+}
